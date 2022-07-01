df7981fde90a4b9648941e8028b2bf0bc0c6aff8
MariaIvanova01 <mimi200801@abv.bg>
2022-07-01 13:30:10 +0300

    Using enumeration.

--- CardRank/src/com/company/Main.java
+++ CardRank/src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(scan.nextLine() + ":");
+
+        Arrays.stream(CardRank.values())
+                .forEach(cardRank -> System.out.printf("Ordinal value: %d; Name value: %s%n", cardRank.ordinal(), cardRank.name()));
     }
 }
